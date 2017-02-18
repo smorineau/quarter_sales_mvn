a1eb23e94844623ad5e6880161abff6bdd8c96ea
Sébastien Morineau <smorineau@gmail.com>
2017-02-18 22:03:13 +0100

    Added creation of first table

---
+++ src/main/sql/01-create-table-quarter-sales.sql
@@ -0,0 +1,7 @@
+CREATE TABLE quarter_sales (
+COUNTRY                       VARCHAR2(20 CHAR),
+Q1_AMOUNT                     NUMBER(6),
+Q2_AMOUNT                     NUMBER(6),
+Q3_AMOUNT                     NUMBER(6),
+Q4_AMOUNT                     NUMBER(6)
+);
